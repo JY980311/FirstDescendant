c0963608c68a369c09c10441bb7d6f67ade0017c
JANG JI YONG <zdy2010@naver.com>
2024-10-17 02:05:34 +0900

    -- 무기 강화 레벨 박스 작업 중

--- app/src/main/java/com/example/firstdescendant/component/WeaponEnchantLevelBox.kt
+++ app/src/main/java/com/example/firstdescendant/component/WeaponEnchantLevelBox.kt
@@ -1,6 +1,25 @@
 package com.example.firstdescendant.component
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 @Composable
-fun WeaponEnchantLevelBox() {}
+fun WeaponEnchantLevelBox(
+    level: Int? = null,
+) {
+    Box(
+        modifier = Modifier
+            .clip(CircleShape)
+            .size(40.dp)
+            .background(Color(0xFFF16626))
+    ){
+
+    }
+}
