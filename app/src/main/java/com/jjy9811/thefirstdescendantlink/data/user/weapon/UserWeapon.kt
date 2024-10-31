9b6ff97a2820349d55821fe4e55ed8c0d5e3c08d
JANG JI YONG <zdy2010@naver.com>
2024-10-31 20:43:49 +0900

    '-- UserWeapon module_capacity = null module_max_capacity = null perk_ability_enchant_level = null 로 수정 '

--- app/src/main/java/com/jjy9811/thefirstdescendantlink/data/user/weapon/UserWeapon.kt
+++ app/src/main/java/com/jjy9811/thefirstdescendantlink/data/user/weapon/UserWeapon.kt
@@ -2,9 +2,9 @@ package com.jjy9811.thefirstdescendantlink.data.user.weapon
 
 data class UserWeapon(
     val module: List<UserWeaponModule>,
-    val module_capacity: Int,
-    val module_max_capacity: Int,
-    val perk_ability_enchant_level: Int?,
+    val module_capacity: Int? = null,
+    val module_max_capacity: Int? = null,
+    val perk_ability_enchant_level: Int? = null,
     val weapon_additional_stat: List<UserWeaponAdditionalStat>?,
     val weapon_id: Int,
     val weapon_level: Int,
